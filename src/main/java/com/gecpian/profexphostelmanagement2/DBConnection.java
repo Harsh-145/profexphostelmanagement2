f37f2b1baa0b449fa367c1b88363e574b5207bf3
YADAV HARSH BALJEETSINGH <gotostudent45@gmail.com>
2026-05-02 00:14:14 +0530

    fix db password

--- src/main/java/com/gecpian/profexphostelmanagement2/DBConnection.java
+++ src/main/java/com/gecpian/profexphostelmanagement2/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // These match your January project credentials
   private static final String URL = "jdbc:mysql://mysql-22a4c18-harsh12345677890.l.aivencloud.com:21545/defaultdb?ssl-mode=REQUIRED";
 private static final String USER = "avnadmin";
-private static final String PASS = "AVNS_PLACEHOLDER";
+private static final String PASS = "AVNS_8rshQhLh6mSKXmPgpBi";
 public static Connection getConnection() throws SQLException, ClassNotFoundException {
         // This line is strictly required for Web Apps to load the MySQL Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
